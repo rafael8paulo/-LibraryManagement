3cb62134bebd10140a7922c6f518c7fd6407f2a9
Vinicius <vbredadj@gmail.com>
2021-11-05 08:11:46 -0300

    Alteracao do projeto para arquitetura MVC

---
+++ src/model/Usuarios.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Usuarios 
+{
+    private int usu_codigo;
+    private String usu_login;
+    private String usu_senha;
+    private LocalDate data;
+    private Privilegios privilegio;
+}
